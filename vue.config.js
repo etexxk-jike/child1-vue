31c91e3e7722cc286b680c8374b4d2269102845f
谢坤 <xiekun@iotsh.com.cn>
2021-06-18 09:15:24 +0800

    [a]

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const { name } = require('./package')
+module.exports = {
+  devServer: {
+    port: 10002,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  },
+  configureWebpack: {
+    output: {
+      library: 'vueApp',
+      libraryTarget: 'umd', // 把微应用打包成 umd 库格式
+      jsonpFunction: `webpackJsonp_${name}`
+    }
+  }
+}
